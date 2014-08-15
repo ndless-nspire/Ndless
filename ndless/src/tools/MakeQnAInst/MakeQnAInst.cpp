235e191d76d7f8ba5996bc2e0921f5f7ee8d437b
Fabian Vogt <fabian@ritter-vogt.de>
2014-08-15 22:08:59 +0200

    Support for all OSs

--- ndless/src/tools/MakeQnAInst/MakeQnAInst.cpp
+++ ndless/src/tools/MakeQnAInst/MakeQnAInst.cpp
@@ -15,10 +15,112 @@ enum OS : int {
 	CXCAS
 };
 
-static uint32_t overflow_addr[4] = { 0x10000000, 0x10000000, 0x1110d843, 0x1116d752 };
-static const uint32_t struct_ptr[4] = { 0x10000000, 0x10000000, 0x10000000, 0x1122d6f4 };
-static const uint32_t loc_code[4] = { 0x10000000, 0x10000000, 0x10000000, 0x1122D004 };
-static uint32_t orig_struct[][32] = {{}, {}, {},
+// Address where the created buffer will be loaded at
+static uint32_t overflow_addr[4] = { 0x10EBB612, 0x10E8F60A, 0x1110D752, 0x1116D752 };
+// Address of a pointer to the struct we want to replace
+static const uint32_t struct_ptr[4] = { 0x10F7B5B4, 0x10F4F5AC, 0x111CD6F4, 0x1122D6F4 };
+// Where the binary (argv[1]) will be put in the buffer (chosen manually, because of unsafe locations)
+static const uint32_t loc_code[4] = { 0x10F7B004, 0x10F4F004, 0x111CD004, 0x1122D004 };
+// The struct itself
+static uint32_t orig_struct[][32] = {
+{
+0x1147A8B4,
+0x1147A944,
+0x1147AA5C,
+0x1147B648,
+0x1147B0B4,
+0x1147BBDC,
+0x1147BCB0,
+0x1147BD84,
+0x1147BF24,
+0x00000001,
+0x1009BDC8,
+0x00008001,
+0x1147C0C4,
+0x1147A9CC,
+0x1147A8E8,
+0x1147A85C,
+0x00000000,
+0x10F7B810,
+0x00000000,
+0x1147A85C,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000
+},
+{
+0x114424FC,
+0x11442294,
+0x114426A4,
+0x11443290,
+0x11442CFC,
+0x11443824,
+0x114438F8,
+0x114439CC,
+0x11443B6C,
+0x00000001,
+0x1009B838,
+0x00008001,
+0x11443D0C,
+0x11442614,
+0x11442530,
+0x114424A4,
+0x00000000,
+0x10F4F808,
+0x00000000,
+0x11442540,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x1144257C,
+},
+{
+0x11767E90,
+0x117E7F08,
+0x11767FE0,
+0x11768BA4,
+0x11768638,
+0x11769110,
+0x117691B4,
+0x11769258,
+0x117693E0,
+0x00000001,
+0x1009B3DC,
+0x00008001,
+0x11769568,
+0x11767F68,
+0x11767EAC,
+0x11767E38,
+0x00000000,
+0x1122D99C,
+0x00000000,
+0x11767EBC,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x11767EF8,
+0x11767E80,
+0x00000000,
+0x1122D99C,
+0x11767BC0,
+0x11767F94,
+0x00000000
+},
 {
 0x11767E90,
 0x117E7F08,
@@ -37,35 +139,27 @@ static uint32_t orig_struct[][32] = {{}, {}, {},
 0x11767EAC,
 0x11767E38,
 0x00000000,
-0x1122d99c,
+0x1122D99C,
 0x00000000,
-0x11767ebc,
-0x00000000,
-0x00000000,
+0x11767EBC,
 0x00000000,
 0x00000000,
 0x00000000,
-0x11767ef8,
-0x11767e80,
 0x00000000,
-0x1122d99c,
-0x11767bc0,
-0x11767f94,
+0x00000000,
+0x11767EF8,
+0x11767E80,
+0x00000000,
+0x1122D99C,
+0x11767BC0,
+0x11767F94,
 0x00000000
 }};
 
-int main(int argc, char **argv)
+std::string luaForOS(OS os, std::string installer_filename)
 {
-	if(argc != 3)
-	{
-		std::cout << "Usage: installer.bin Problem1.xml" << std::endl;
-		return 1;
-	}
-
-	int os = OS::CXCAS;
-
-	//Build the buffer content
-	std::ifstream installer_bin(argv[1], std::ios::binary);
+  	//Build the buffer content
+	std::ifstream installer_bin(installer_filename, std::ios::binary);
 
 	std::vector<uint8_t> toplevel_pad(4 - (overflow_addr[os] & 3));
 	overflow_addr[os] = (overflow_addr[os] + 4) & ~3;
@@ -90,8 +184,8 @@ int main(int argc, char **argv)
 
 	if(ptr_pos < buffer_content.size())
 	{
-		std::cerr << "Binary too large!" << std::endl;
-		return 1;
+		std::cerr << "Binary too large (Binary ends at 0x" << std::hex << buffer_content.size() + overflow_addr[os] << ", but the struct ptr is at 0x" << std::hex << struct_ptr[os]  << "!" << std::endl;
+		return "";
 	}
 
 	buffer_content.resize(ptr_pos);
@@ -105,12 +199,10 @@ int main(int argc, char **argv)
 	//The buffer mustn't contain any 0s
 	std::replace(buffer_content.begin(), buffer_content.end(), 0, 1);
 
-	std::cout << "Size of buffer: 0x" << std::hex << buffer_content.size() << std::endl;
+	std::cout << "Size of buffer (OS " << os << "): 0x" << std::hex << buffer_content.size() << std::endl;
 
 	//Now generate a lua formatted string
 	std::ostringstream ss(std::ios_base::ate);
-	ss.str("u = string.uchar\ns = \"\"\n");
-
 	std::ostringstream line(std::ios_base::ate);
 	line.str("s = s..\"");
 
@@ -120,7 +212,7 @@ int main(int argc, char **argv)
 		auto j = std::adjacent_find(i, buffer_content.end(), std::not_equal_to<uint8_t>());
 		int count = std::distance(i, j) + (j == buffer_content.end() ? 0 : 1);
 
-		if(count < 8)
+		if(count < 199999)
 			count = 1;
 
 		if(count == 1)
@@ -131,7 +223,7 @@ int main(int argc, char **argv)
 			line.str("s = s..string.rep(\"\\"); line << (int)*i << "\", " << count << ")..\"";
 		}
 
-		if(line.tellp() >= 320 || i + count == buffer_content.end())
+		if(line.tellp() >= 15000000 || i + count == buffer_content.end())
 		{
 			ss << line.str() << "\"" << std::endl;
 			line.str("s = s..\"");
@@ -139,22 +231,44 @@ int main(int argc, char **argv)
 
 		i += count;
 	}
+	
+	return ss.str();
+}
+
+int main(int argc, char **argv)
+{
+	if(argc != 3)
+	{
+		std::cout << "Usage: installer.bin Problem1.xml" << std::endl;
+		return 1;
+	}
 
 	//Write LUA
 	std::ostringstream lua;
 
-	lua << ss.str() << std::endl;
-
-	lua << "on = {}" << std::endl
+	lua 	<< "on = {}" << std::endl
+		<< "local _, caserr = math.eval(\"solve()\")" << std::endl
+		<< "cas = caserr == 930" << std::endl
+		<< "cas = not (math.eval(\"solve()\") == 930)" << std::endl
 		<< "function on.restore()" << std::endl
+		<< "s = \"\"" << std::endl
+		<< "local cx = platform.isColorDisplay()" << std::endl
+		<< "if not cas and not cx then" << std::endl
+		<< luaForOS(OS::NCAS, argv[1])
+		<< "elseif cas and not cx then" << std::endl
+		<< luaForOS(OS::CAS, argv[1])
+		<< "elseif not cas and cx then" << std::endl
+		<< luaForOS(OS::CX, argv[1])
+		<< "else" << std::endl
+		<< luaForOS(OS::CXCAS, argv[1])
+		<< "end" << std::endl
 		<< "tiassert.assert(false, 0, s)" << std::endl
 		<< "end" << std::endl
 		<< "return {QuestionProperties={prompt=\"\"}, DropZone={vScreen={}}}" << std::endl;
 
 	//XML-Escape LUA
 	std::string lua_escaped = lua.str();
-
-	size_t pos;
+	size_t pos = 0;
 	while((pos = lua_escaped.find("\"", pos)) != std::string::npos)
 	{
 		lua_escaped.replace(pos, 1, "&quot;");
