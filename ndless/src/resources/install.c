d50046eb786bead2fc2e85747aad479c889ff6df
Howf1 <47375241+Howf1@users.noreply.github.com>
2019-02-05 21:21:39 -0600

    Update install.c

--- ndless/src/resources/install.c
+++ ndless/src/resources/install.c
@@ -233,9 +233,6 @@ HOOK_DEFINE(ins_successsuccessmsg_hook) {
 	// OS-specific: reg number
 	if (HOOK_SAVED_REGS(ins_successsuccessmsg_hook)[2] == ins_successmsg_icon[ut_os_version_index]) {
 		Gc gc = (Gc)HOOK_SAVED_REGS(ins_successsuccessmsg_hook)[0];
-		gui_gc_setColor(gc, has_colors ? 0x32cd32 : 0x505050);
-		gui_gc_setFont(gc, SerifRegular9);
-		gui_gc_drawString(gc, (char*) u"Ndless installed!", 25, 4, GC_SM_TOP);
 	}
 	HOOK_RESTORE_RETURN(ins_successsuccessmsg_hook);
 }
