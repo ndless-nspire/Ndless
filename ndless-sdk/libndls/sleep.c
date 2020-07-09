84b8b9fcf3f8fb95ff1b4dced7f9e87fa98ff1ee
Fabian Vogt <fabian@ritter-vogt.de>
2020-07-09 23:36:03 +0200

    Implement msleep based on the interrupt status
    Reading the value directly after writing to load is racy, it might not have
    updated yet. The only way to do this in a race-free way is by using the sticky
    interrupt flag, which is unfortunately reset by idle().
    
    Fixes #207

--- ndless-sdk/libndls/sleep.c
+++ ndless-sdk/libndls/sleep.c
@@ -38,18 +38,31 @@ unsigned msleep(unsigned millisec) {
 		*timer = 32;
 	} else {
 		// see http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0271d/CHDFDDCF.html
-		volatile unsigned *control = (unsigned*)0x900D0008;
 		volatile unsigned *load = (unsigned*)0x900D0000;
-		volatile unsigned *value = (unsigned*)0x900D0004;
+		volatile unsigned *control = (unsigned*)0x900D0008;
+		volatile unsigned *int_clear = (unsigned*)0x900D000C;
+		volatile unsigned *int_status = (unsigned*)0x900D0010;
 		unsigned orig_control = *control;
 		unsigned orig_load = *load;
 		*control = 0; // disable timer
-		*control = 0b01100011; // disabled, TimerMode N/A, int, no prescale, 32-bit, One Shot (for the *value > 0 test) -> 32khz
-		*control = 0b11100011; // enable timer
+		*int_clear = 1; // clear interrupt status
 		*load = 32 * millisec;
-		while (*value > 0)
-			idle();
+		*control = 0b01100011; // disabled, TimerMode N/A, int, no prescale, 32-bit, One Shot -> 32khz
+		*control = 0b11100011; // enable timer
+
+		// Can't use idle() here as it acks the timer interrupt
+		volatile unsigned *intmask = IO(0xDC000008, 0xDC000010);
+		unsigned orig_mask = intmask[0];
+		intmask[1] = ~(1 << 19); // Disable all IRQs except timer
+
+		while ((*int_status & 1) == 0)
+			__asm volatile("mcr p15, 0, %0, c7, c0, 4" : : "r"(0) ); // Wait for an interrupt to occur
+
+		intmask[1] = 0xFFFFFFFF; // Disable all IRQs
+		intmask[0] = orig_mask; // renable IRQs
+
 		*control = 0; // disable timer
+		*int_clear = 1; // clear interrupt status
 		*control = orig_control & 0b01111111; // timer still disabled
 		*load = orig_load;
 		*control = orig_control; // enable timer
@@ -57,3 +70,4 @@ unsigned msleep(unsigned millisec) {
 	
 	return 0;
 }
+
