23564dbb7565d5ee3524b0b1bf6be8778e024377
gameblabla <gameblabla@openmailbox.org>
2020-11-02 01:27:58 +0100

    Don't search fileystem for ndless.cfg every time. Rebase https://github.com/ndless-nspire/Ndless/pull/123/commits/8cafb6e9b41e33d301e39f7f0c0a761dff124437

--- ndless-sdk/libndls/config.c
+++ ndless-sdk/libndls/config.c
@@ -120,7 +120,15 @@ close_quit:
 }
 
 static int cfg_locate_cfg_file(char *dst_path, size_t dst_path_size) {
-	return locate("ndless.cfg.tns", dst_path, dst_path_size);
+	static char cfg_path[FILENAME_MAX] = {0};
+	if(cfg_path[0] == 0) snprintf(cfg_path, FILENAME_MAX, "%s%s", get_documents_dir(), "ndless/ndless.cfg.tns");
+	if(access(cfg_path, F_OK) == -1) {
+		int l = locate("ndless.cfg.tns", cfg_path, FILENAME_MAX);
+		if(l != 0) return l;
+	}
+	if(strlen(cfg_path)+1 > dst_path_size) return 1;
+	strlcpy(dst_path, cfg_path, dst_path_size);
+	return 0;
 }
 
 void cfg_open(void) {
