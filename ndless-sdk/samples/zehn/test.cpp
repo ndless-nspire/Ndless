eb8317548e8b5c2ef067d7093e1f2e479d87cda9
Fabian Vogt <fabian@ritter-vogt.de>
2024-05-25 17:36:59 +0200

    Support .init_array and .fini_array sections with priorities
    * Add support to the linker script and startup code.
    * Add tests for init_priority to the zehn sample

--- ndless-sdk/samples/zehn/test.cpp
+++ ndless-sdk/samples/zehn/test.cpp
@@ -4,17 +4,27 @@
 
 using namespace std;
 
+// Test global object construcion and order.
+// This should work both with -fuse-cxa-atexit (the default)
+// and -fno-use-cxa-atexit.
+template <int i>
 class Test
 {
 public:
-	Test() { cout << "Test();" << endl; }
-	~Test() { cout << "~Test();" << endl; }
+	Test() { cout << "Test(" << i << ")" << endl; }
+	~Test() {
+		cout << "~Test(" << i << ")" << endl;
+		if (i == 1000) // Should be last
+			wait_key_pressed();
+	}
 
 	void doSomething() { cout << "Doing something!" << endl; }
 };
 
-//Test global variables + initialization
-static Test test;
+static __attribute__ ((init_priority (1000))) Test<1000> t1000;
+static __attribute__ ((init_priority (1001))) Test<1001> t1001;
+// No init_priority is means higher than any init_priority
+static Test<1024> test;
 
 int main()
 {
